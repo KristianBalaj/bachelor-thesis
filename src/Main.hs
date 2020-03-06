cff6644723b73023bb3ab2e669f971e69a50e232
Kristian Balaj <kiko.balaj@gmail.com>
2020-03-06 17:14:17 +0100

    parsing codons file

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,23 @@
+{-# LANGUAGE DeriveGeneric #-}
 module Main where
 
+import System.IO
+import Data.Aeson
+import GHC.Generics
+import qualified Data.ByteString.Lazy as B
+
+data Codons = Codons {
+    nonpolar :: [String],
+    polar :: [String],
+    basic :: [String],
+    acidic :: [String],
+    termination :: [String]
+} deriving (Show, Generic)
+
+instance FromJSON Codons
+
+parseCodons :: FilePath -> IO (Maybe Codons)
+parseCodons path = fmap decode (B.readFile path)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = parseCodons "codons.json" >>= print
